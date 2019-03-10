0c786577118717add19c637b3d465a6a02852095
Miguel <tsambamiguelito@gmail.com>
2019-03-10 23:59:20 +0100

    Add full component files

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,18 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css'],
 })
 export class AppComponent {
-  title = 'CalculateAngular';
+number1: number ;
+number2: number;
+sum:number;
+multiplicate: number;
+substract:number;
+division:number;
+current = (new Date()).getFullYear();
+
+onSubmit() {
+this.sum = this.number1 + this.number2;
+this.multiplicate = this.number1 * this.number2;
+this.substract = this.number1 - this.number2;
+this.division = this.number1 / this.number2;
+}
 }
